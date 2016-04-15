8c4ab3c4cee6be67d3b6ba86f3ea2b7d3b9b97ef
cmneville <cmneville@180CMNEVILLE.hcps.internal>
2016-04-15 12:07:08 -0400

    Added Test Class and made slight adjustments to the framework.

---
+++ src/draw/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package draw;
+import edu.princeton.cs.introcs.*;
+/**
+ *
+ * @author cmneville
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(1000,1000);//sets the size of the window
+        
+    }
+    
+}
